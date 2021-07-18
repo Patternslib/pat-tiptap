10264e9b7585f612f56ad6bec5f4f6a64827cd27
Johannes Raggam <thetetet@gmail.com>
2021-07-19 00:35:48 +0200

    Document how to use Vue.

--- webpack.vue.js
+++ webpack.vue.js
@@ -1,3 +1,22 @@
+// Depend on (snippet from package.json):
+// {
+//     "dependencies": {
+//         "vue": "^2.6.12",
+//         "vue-async-computed": "^3.9.0"
+//     },
+//     "devDependencies": {
+//         "vue-jest": "^4.0.1",
+//         "vue-loader": "^15.9.6",
+//         "vue-template-compiler": "^2.6.12"
+//     }
+// }
+//
+// Use in webpack like:
+// const vue_config = require("./webpack.vue");
+// module.exports = async (env, argv) => {
+//     const config = vue_config(env, argv, patternslib_config(env, argv));
+//     [...]
+// }
 const VueLoaderPlugin = require("vue-loader/lib/plugin");
 
 module.exports = (env, argv, config) => {
