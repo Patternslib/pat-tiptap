7d92553a3d042e6c1908c5928ddaa91315ce6171
Johannes Raggam <thetetet@gmail.com>
2021-08-18 19:38:41 +0200

    Upgrade to tiptap v2.

--- src/tiptap.js
+++ src/tiptap.js
@@ -11,51 +11,38 @@ export default Base.extend({
     trigger: ".pat-tiptap",
 
     async init() {
-        const Editor = (await import("@tiptap/core")).Editor;
+        const TipTap = (await import("@tiptap/core")).Editor;
         const StarterKit = (await import("@tiptap/starter-kit")).default;
 
-        new Editor({
-            element: this.el,
+        this.options = parser.parse(this.el, this.options);
+
+        // Hide element which will be replaced with tiptap instance
+        this.el.style.display = "none";
+        // Create container for tiptap
+        const container = document.createElement("div");
+        container.setAttribute("class", "tiptap-container");
+        this.el.after(container);
+
+        const is_form_el = ["TEXTAREA", "INPUT"].includes(this.el.tagName);
+
+        const getText = () => {
+            // Textarea value getter
+            return is_form_el ? this.el.value : this.el.innerHTML;
+        };
+
+        const setText = (text) => {
+            // Textarea value setter
+            if (is_form_el) {
+                this.el.value = text;
+            } else {
+                this.el.innerHTML = text;
+            }
+        };
+
+        const editor = new TipTap({
+            element: container,
             extensions: [StarterKit],
+            content: getText(),
         });
-
-        //const Vue = (await import("vue")).default;
-        //const VueAsyncComputed = (await import("vue-async-computed")).default;
-        //const Editor = (await import("./tiptap-editor.vue")).default;
-
-        //this.options = parser.parse(this.el, this.options);
-
-        //// Hide element which will be replaced with tiptap instance
-        //this.el.style.display = "none";
-
-        //const is_form_el = ["TEXTAREA", "INPUT"].includes(this.el.tagName);
-
-        //const getText = () => {
-        //    // Textarea value getter
-        //    return is_form_el ? this.el.value : this.el.innerHTML;
-        //};
-
-        //const setText = (text) => {
-        //    // Textarea value setter
-        //    if (is_form_el) {
-        //        this.el.value = text;
-        //    } else {
-        //        this.el.innerHTML = text;
-        //    }
-        //};
-
-        //// Initialize editor
-        //Vue.use(VueAsyncComputed); // Allow ``async`` for computed properties.
-        //const editor_app = new Vue({
-        //    render: (h) =>
-        //        h(Editor, {
-        //            props: {
-        //                getText: getText,
-        //                setText: setText,
-        //                options: this.options,
-        //            },
-        //        }),
-        //}).$mount();
-        //this.el.parentNode.insertBefore(editor_app.$el, this.el);
     },
 });
