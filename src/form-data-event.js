587d3ed456eb3faf76bacbbd69e4fa79682b7f2d
Johannes Raggam <thetetet@gmail.com>
2021-09-01 14:47:30 +0200

    form-data-event: support initializing form elements with data via an initialization-event-name.

--- src/form-data-event.js
+++ src/form-data-event.js
@@ -6,6 +6,7 @@ const log = logging.getLogger("form-data-event");
 
 export const parser = new Parser("form-data-event");
 parser.addArgument("event-name");
+parser.addArgument("initialization-event-name");
 parser.addArgument("prevent-submit", true);
 parser.addArgument("close-modal", true);
 
@@ -20,21 +21,42 @@ export default Base.extend({
             return;
         }
         this.el.addEventListener("submit", this.handle_submit.bind(this));
+        if (this.options.initializationEventName) {
+            document.addEventListener(
+                this.options.initializationEventName,
+                this.handle_init.bind(this)
+            );
+        }
     },
 
     handle_submit(e) {
         if (this.options.preventSubmit) {
             e.preventDefault();
         }
-        const data = new FormData(this.el);
+        const form_data = new FormData(this.el);
         const ev = new CustomEvent(this.options.eventName, {
-            detail: { form_data: data },
+            detail: { form_data: form_data },
         });
         document.dispatchEvent(ev);
         if (this.options.closeModal) {
             const modal = this.el.closest(".pat-modal");
             if (modal) {
                 modal["pattern-modal"].destroy();
+            }
+        }
+        if (this.options.initializationEventName) {
+            document.removeEventListener(
+                this.options.initializationEventName,
+                this.handle_init
+            );
+        }
+    },
+
+    handle_init(e) {
+        const form_data = e?.detail?.form_data;
+        for (const [key, value] of form_data.entries()) {
+            if (this.el[key]) {
+                this.el[key].value = value;
             }
         }
     },
