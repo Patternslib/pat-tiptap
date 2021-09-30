20e2d9a234af25fa91837dd8ade2a7002fbfdbc3
Johannes Raggam <thetetet@gmail.com>
2021-09-30 15:19:01 +0200

    Apply pattern on mentions context menu.

--- src/extensions/mentions.js
+++ src/extensions/mentions.js
@@ -2,10 +2,30 @@ import { Node, mergeAttributes } from "@tiptap/core";
 import { PluginKey } from "prosemirror-state";
 import { context_menu, context_menu_close } from "../context_menu";
 
+function pattern_mentions_context_menu(plugin_context) {
+    // Dynamic pattern for the mentions context menu
+    return {
+        name: "tiptap-mentions-context-menu",
+        trigger: ".tiptap-mentions-context-menu",
+        async init($el) {
+            $el.on("submit", (e) => {
+                // We need jQuery here:
+                // 1) pat-autosubmit submits via $(form).submit(); which cannot be catched by addEventListener
+                // 2) Safari (and IE) do not support form.requestSubmit()
+                //    (which would dispatch a "submit" - in contrast to form.submit())
+                //    nor the Submit() event.
+                e.preventDefault();
+                context_menu_close(this.name);
+                plugin_context.editor.commands.setNode("mentions");
+            });
+        },
+    };
+}
+
 export const MentionsPluginKey = new PluginKey("mentions");
 
 export const Mentions = Node.create({
-    name: "mention",
+    name: "mentions",
 
     defaultOptions: {
         HTMLAttributes: {},
@@ -74,7 +94,12 @@ export const Mentions = Node.create({
     addKeyboardShortcuts() {
         return {
             "@": () => {
-                context_menu(this.options.url, this.editor);
+                context_menu(
+                    this.options.url,
+                    this.editor,
+                    undefined,
+                    pattern_mentions_context_menu(this)
+                );
             },
         };
     },
