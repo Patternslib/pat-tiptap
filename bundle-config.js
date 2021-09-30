79071f973095b5db3948d7adfcde0649e2a8b0c1
Johannes Raggam <thetetet@gmail.com>
2021-09-30 09:50:05 +0200

    Improve styling a bit.

--- bundle-config.js
+++ bundle-config.js
@@ -2,4 +2,7 @@ import registry from "@patternslib/patternslib/src/core/registry";
 import "@patternslib/patternslib/src/pat/modal/modal";
 import "./src/tiptap";
 
+// Importing pattern styles in JavaScript
+window.__patternslib_import_styles = true;
+
 registry.init();
