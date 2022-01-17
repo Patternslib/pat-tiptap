bf40edefe003bfc52cd8a6e0353030ab1b28cf64
Johannes Raggam <thetetet@gmail.com>
2022-01-17 09:57:15 +0100

    maint: Fix tests.

--- src/tiptap.test.js
+++ src/tiptap.test.js
@@ -7,29 +7,25 @@ describe("pat-tiptap", () => {
         document.body.innerHTML = "";
     });
 
-    it("is initialized correctly on textarea elements", async (done) => {
+    it("is initialized correctly on textarea elements", async () => {
         document.body.innerHTML = `<textarea class="pat-tiptap">hello</textarea>`;
 
         pattern.init(document.querySelector(".pat-tiptap"));
         await utils.timeout(1);
 
         expect(document.querySelector(".pat-tiptap").style.display).toBe("none"); // prettier-ignore
-        expect(document.querySelector(".tiptap-editor *[contenteditable]").textContent).toBe("hello"); // prettier-ignore
-        expect(document.querySelector(".tiptap-editor *[contenteditable]").innerHTML).toBe("<p>hello</p>"); // prettier-ignore
-
-        done();
+        expect(document.querySelector(".tiptap-container *[contenteditable]").textContent).toBe("hello"); // prettier-ignore
+        expect(document.querySelector(".tiptap-container *[contenteditable]").innerHTML).toBe("<p>hello</p>"); // prettier-ignore
     });
 
-    it("is initialized correctly on div elements", async (done) => {
+    it("is initialized correctly on div elements", async () => {
         document.body.innerHTML = `<div class="pat-tiptap" contenteditable>hello</div>`;
 
         pattern.init(document.querySelector(".pat-tiptap"));
         await utils.timeout(1);
 
         expect(document.querySelector(".pat-tiptap").style.display).toBe("none"); // prettier-ignore
-        expect(document.querySelector(".tiptap-editor *[contenteditable]").textContent).toBe("hello"); // prettier-ignore
-        expect(document.querySelector(".tiptap-editor *[contenteditable]").innerHTML).toBe("<p>hello</p>"); // prettier-ignore
-
-        done();
+        expect(document.querySelector(".tiptap-container *[contenteditable]").textContent).toBe("hello"); // prettier-ignore
+        expect(document.querySelector(".tiptap-container *[contenteditable]").innerHTML).toBe("<p>hello</p>"); // prettier-ignore
     });
 });
