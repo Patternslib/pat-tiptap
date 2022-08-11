34b16e53eeaf01d9387f9d11f77b4a2732b0337f
Johannes Raggam <thetetet@gmail.com>
2022-08-11 17:35:34 +0200

    maint(Build): Include bundle name and version in generated files (Feature of @patternslib/dev 2.4.0.)

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,20 @@ process.traceDeprecation = true;
 const mf_config = require("@patternslib/dev/webpack/webpack.mf");
 const package_json = require("./package.json");
 const path = require("path");
-const patternslib_config = require("@patternslib/dev/webpack/webpack.config.js");
 const patternslib_package_json = require("@patternslib/patternslib/package.json");
+const webpack_config = require("@patternslib/dev/webpack/webpack.config").config;
 
-module.exports = async (env, argv) => {
+module.exports = () => {
     let config = {
         entry: {
             "bundle.min": path.resolve(__dirname, "index.js"),
         },
     };
 
-    config = patternslib_config(env, argv, config);
+    config = webpack_config({
+        config: config,
+        package_json: package_json,
+    });
     config.output.path = path.resolve(__dirname, "dist/");
 
     config.plugins.push(
